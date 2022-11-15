6d7cd956c9d3b1940e76fca7bc547a4e45761e95
sebastienmoulin <sebastien.m.web@gmail.com>
2022-11-15 14:31:46 +0100

    Finish

--- app.js
+++ app.js
@@ -14,15 +14,18 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
 const users = require("./userHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
 
 app.get("/api/users", users.getUsers);
 app.get("/api/users/:id", users.getUserById);
 app.post("/api/users", users.postUser);
+app.put("/api/users/:id", userHandlers.putUser);
 
 app.listen(port, (err) => {
   if (err) {
--- userHandlers.js
+++ userHandlers.js
@@ -47,8 +47,31 @@ const postUser = (req, res) => {
     });
 };
 
+const putUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city } = req.body;
+
+  database
+    .query(
+      "UPDATE users SET firstname = ?, lastname = ?, email = ?, city = ? WHERE id = ?",
+      [firstname, lastname, email, city, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the user");
+    });
+};
+
 module.exports = {
   getUsers,
   getUserById,
   postUser,
+  putUser,
 };
